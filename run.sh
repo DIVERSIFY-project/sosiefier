34c04a472320e3295c37f1fead751dd15b55eb0b
Simon Allier <simon.allier@gmail.com>
2013-05-31 16:58:52 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,33 +1,64 @@
 #!/bin/sh
 
-java -Xmx2000m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
 killall java
 
-java -Xmx2000m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
 killall java
 
-java -Xmx2000m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
 killall java
 
-java -Xmx2000m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
 killall java
 
-java -Xmx2000m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
 killall java
 
-java -Xmx2000m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
 killall java
 
-java -Xmx2000m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
 killall java
 
-java -Xmx2000m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
 killall java
 
-java -Xmx2000m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
 killall java
 
-java -Xmx2000m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
 killall java
+
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+killall java
+
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+killall java
+
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+killall java
+
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+killall java
+
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+killall java
+
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+killall java
+
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+killall java
+
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+killall java
+
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+killall java
+
+java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar -src $1 -jacoco $2 -nbRun $3 -out $4
+killall java
+
 
 
