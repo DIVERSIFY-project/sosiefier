831937c440a1c256d296d602d19ad1e0ca513206
Simon Allier <simon.allier@gmail.com>
2013-09-06 11:23:27 +0200

    $3

---
+++ runFromGit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+java -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar git $2
+
+for i in `seq 1 $1`
+do
+    java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar $(cat propertiesFile)
+done
