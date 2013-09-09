8e645c9e4c083146a097f9d05457732de54cef8a
Simon Allier <simon.allier@gmail.com>
2013-09-09 15:28:12 +0200

    $3

---
+++ src/main/java/fr/inria/diversify/transformation/MavenInvocationResult.java
@@ -0,0 +1,9 @@
+package fr.inria.diversify.transformation;
+
+/**
+ * User: Simon
+ * Date: 9/9/13
+ * Time: 3:20 PM
+ */
+public class MavenInvocationResult {
+}
---
+++ src/main/java/fr/inria/diversify/transformation/MavenInvoker.java
@@ -0,0 +1,9 @@
+package fr.inria.diversify.transformation;
+
+/**
+ * User: Simon
+ * Date: 9/9/13
+ * Time: 3:15 PM
+ */
+public class MavenInvoker {
+}
