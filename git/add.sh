86bb89801be1174876a02a1fe4e4409294395c0c
Simon Allier <simon.allier@gmail.com>
2013-09-10 18:10:57 +0200

    update grid5000.sh

--- git/add.sh
+++ git/add.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 cd $1
-
+git pull
 git add $2
+
+
+git commit -m 'update'
+git push --repo https://diversify-exp-user:diversify-exp-password@github.com/simonAllier/diversify-exp.git
--- git/push.sh
+++ git/push.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd $1
-
-git commit -m 'update'
-git push --repo https://diversify-exp-user:diversify-exp-password@github.com/simonAllier/diversify-exp.git
+#cd $1
+#git commit -m 'update'
+#git push --repo https://diversify-exp-user:diversify-exp-password@github.com/simonAllier/diversify-exp.git
