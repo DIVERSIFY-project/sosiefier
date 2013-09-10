2aa7ad972d3766e3526411149d4a865ec83e3e2d
Simon Allier <simon.allier@gmail.com>
2013-09-10 09:18:49 +0200

    update grid5000.sh

--- grid5000.sh
+++ grid5000.sh
@@ -2,4 +2,7 @@
 cd /root/diversify-statements
 git pull
 mvn clean package
-sh runFromGit.sh 100 repo
+java -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar git $2
+
+sh runFromGit.sh 100 &
+sh runFromGit.sh 100 &
--- runFromGit.sh
+++ runFromGit.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-java -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar git $2
 
 for i in `seq 1 $1`
 do
-    java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar $(cat propertiesFile) &
     java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar $(cat propertiesFile)
 done
