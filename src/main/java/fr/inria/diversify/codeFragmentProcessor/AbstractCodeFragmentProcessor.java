bc5c90e4bc720db5afd4190764aa1851d98dca9e
Simon Allier <simon.allier@gmail.com>
2013-09-23 17:12:36 +0200

    update

---
+++ src/main/java/fr/inria/diversify/codeFragmentProcessor/AbstractCodeFragmentProcessor.java
@@ -0,0 +1,11 @@
+package fr.inria.diversify.codeFragmentProcessor;
+
+import spoon.processing.AbstractProcessor;
+
+/**
+ * User: Simon
+ * Date: 9/23/13
+ * Time: 2:23 PM
+ */
+public abstract class AbstractCodeFragmentProcessor<E> extends AbstractProcessor<E> {
+}
