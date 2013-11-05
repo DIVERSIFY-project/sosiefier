59b6a6c080232c9c8837a23aaec0b9423a01de6f
Simon Allier <simon.allier@gmail.com>
2013-11-05 14:37:55 +0100

    fix in pom

--- script/runFromGit.sh
+++ script/runFromGit.sh
@@ -5,9 +5,8 @@ for i in `seq 1 $1`
 do
     for j in `seq 1 $cpu`
     do
-        java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar $(cat propertiesFile) >  out$i$j &
+        java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar $(cat propertiesFile) >  out_$i_$j &
     done
     wait
-    killall java
     sh git/push.sh $2/diversify-exp
 done
