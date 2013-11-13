e5a83ea1198c9a5ee92f53d1c7715ca2b262f6e9
Simon Allier <simon.allier@gmail.com>
2013-11-13 11:08:11 +0100

    add new CodeFragmentList

---
+++ src/main/java/fr/inria/diversify/CodeFragmentList2.java
@@ -0,0 +1,66 @@
+package fr.inria.diversify;
+
+import fr.inria.diversify.codeFragment.CodeFragment;
+import spoon.reflect.declaration.CtSimpleType;
+
+import java.util.*;
+
+/**
+ * User: Simon
+ * Date: 13/11/13
+ * Time: 10:57
+ */
+public class CodeFragmentList2 extends LinkedList<CodeFragment> {
+    protected Map<Integer,Integer> uniqueCodeFragment;
+    protected Map<Integer,CodeFragment> idToCodeFragment;
+    private Collection<CodeFragment> collection;
+
+    public CodeFragmentList2(List<CodeFragment> codeFragments) {
+        uniqueCodeFragment = new HashMap<Integer, Integer>();
+        idToCodeFragment = new HashMap<Integer,CodeFragment>();
+        this.addAll(codeFragments);
+
+    }
+
+    public List<CtSimpleType> getAllClasses() {
+        List<CtSimpleType> classes = new ArrayList<CtSimpleType>();
+
+        for(CodeFragment cf: this)
+            if(!classes.contains(cf.getSourceClass()))
+                classes.add(cf.getSourceClass());
+        return classes;
+    }
+
+    public Map<Integer, Integer> getUniqueCodeFragments() {
+        return uniqueCodeFragment;
+    }
+
+    public boolean add(CodeFragment cf) {
+        addUniqueStatement(cf);
+
+        return super.add(cf);
+    }
+
+    public boolean addAll(Collection<? extends CodeFragment> collection) {
+        boolean ret = false;
+        for (CodeFragment cf : collection)
+            ret = ret || add(cf);
+        return ret;
+    }
+
+    protected void addUniqueStatement(CodeFragment cf) {
+        boolean same = false;
+        for (Integer otherCfId: uniqueCodeFragment.keySet()) {
+            if(otherCfId == cf.id()) {
+                same = true;
+                uniqueCodeFragment.put(otherCfId, uniqueCodeFragment.get(otherCfId)+1);
+                break;
+            }
+        }
+        if(!same) {
+            uniqueCodeFragment.put(cf.id(),1);
+            idToCodeFragment.put(cf.id(), cf);
+        }
+    }
+
+}
