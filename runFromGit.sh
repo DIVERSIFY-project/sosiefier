7733d993843d21432ca084a6e45e25fd337b9511
Simon Allier <simon.allier@gmail.com>
2013-12-13 11:15:15 +0100

    add http proxy in runFromGit

--- runFromGit.sh
+++ runFromGit.sh
@@ -5,7 +5,7 @@ do
     for j in `seq 1 $cpu`
     do
         sleep 2
-        java -Xmx2000m -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar $(cat propertiesFile) >  out_$i_$j &
+        java -Xmx2000m -Dhttp.proxyHost=proxy.rennes.grid5000.fr -Dhttp.proxyPort=3128 -XX:MaxPermSize=256m -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar $(cat propertiesFile) >  out_$i_$j &
     done
     wait
     sh script/git/push.sh $2/diversify-exp
