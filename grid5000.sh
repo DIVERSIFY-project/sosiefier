9b16144a40f71f22bec8c6949b7343ac591bbbbd
Simon Allier <simon.allier@gmail.com>
2013-12-17 14:33:08 +0100

    update grid5000.sh

--- grid5000.sh
+++ grid5000.sh
@@ -3,8 +3,8 @@ cd /root/diversify-statements
 git pull
 mvn clean package
 
-n=$RANDOM
-sleep $(( n %= 300 ))
+
+sleep $((RANDOM%300))
 
 rm -rf repo/diversify-exp
 java -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar git repo
