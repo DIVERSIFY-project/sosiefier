de432c9187ad7ef2705e835a2a5bdfb3c0591839
Simon Allier <simon.allier@gmail.com>
2013-12-18 14:08:59 +0100

    fix in grid5000

--- grid5000.sh
+++ grid5000.sh
@@ -3,10 +3,11 @@ cd /root/diversify-statements
 git pull
 mvn clean package
 
-
-sleep $((RANDOM%300))
-
 rm -rf repo/diversify-exp
+sh script/git/init.sh repo
+sleep $((RANDOM%500))
+
+
 java -jar target/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar git repo
 
 sh runFromGit.sh 100 &
