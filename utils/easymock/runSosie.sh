e6db1fe7d3986fbd67c42f7c4b3e1edfb0be1cb5
Simon Allier <simon.allier@gmail.com>
2014-03-06 17:39:22 +0100

    add cvl properties files

--- utils/easymock/runSosie.sh
+++ utils/easymock/runSosie.sh
@@ -31,26 +31,12 @@ do
    echo project=$3 >> ../compare.properties
     echo result=$4 >> ../compare.properties
     echo varToExclude=$5 >> ../compare.properties
-     echo type=assert >> ../compare.properties
+     echo type=all >> ../compare.properties
     echo dirOriginal=../log/$sosie/mutant >> ../compare.properties
     echo dirSosie=../log/$sosie/mutantSosie >> ../compare.properties
 
-echo "run assert compare"
 cat ../compare.properties
    java -Xmx2000m -XX:MaxPermSize=256m -cp $7/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.diversify.sosie.compare.refactor.CompareLogMain ../compare.properties
-
-   rm ../compare.properties
-    cp $2 ../compare.properties
-     echo project=$3 >> ../compare.properties
-    echo result=$4 >> ../compare.properties
-    echo varToExclude=$6 >> ../compare.properties
-     echo type=exception >> ../compare.properties
-    echo dirOriginal=../log/$sosie/mutant >> ../compare.properties
-    echo dirSosie=../log/$sosie/mutantSosie >> ../compare.properties
-
-echo "run exception compare"
-cat ../compare.properties
-   java -Xmx2000m -XX:MaxPermSize=256m -cp $7/Diversify-statements-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.diversify.sosie.compare.CompareLogMain ../compare.properties
 
 done
 
