0e7be30da6f55d6a43317d55b78bdae12b208384
Simon Allier <simon.allier@gmail.com>
2014-08-12 14:19:20 +0200

    fix for compare

--- src/main/java/fr/inria/diversify/sosie/compare/CompareStackTrace.java
+++ src/main/java/fr/inria/diversify/sosie/compare/CompareStackTrace.java
@@ -19,68 +19,19 @@ public class CompareStackTrace extends AbstractCompareStackTrace {
     public List<Diff> findDiff() {
         List<Diff> diffs = new LinkedList();
 
-//        boolean st1Lower = false, st2Lower = false;
-//        while(originalStackTrace.hasNext() && sosieStackTrace.hasNext()) {
-//            if (!(originalStackTrace.nextIsVar() || sosieStackTrace.nextIsVar())) {
-//                if (!st1Lower) {
-//                    originalStackTrace.next();
-//                }
-//                if (!st2Lower) {
-//                    sosieStackTrace.next();
-//                }
-//            } else {
-//                if(originalStackTrace.nextIsVar()) {
-//                    nextVar(originalStackTrace, sosieStackTrace.getStartLogging());
-////                    originalStackTrace.next();
-//                }
-//                if(sosieStackTrace.nextIsVar()) {
-//                    nextVar(sosieStackTrace, sosieStackTrace.getStartLogging());
-////                    sosieStackTrace.next();
-//                }
-//            }
-//
-//            StackTraceCall top1 = originalStackTrace.getTop();
-//            StackTraceCall top2 = sosieStackTrace.getTop();
-//            int deep1 = originalStackTrace.getDeep();
-//            int deep2 = sosieStackTrace.getDeep();
-//
-//            if(!st1Lower && deep1 < deep2) {
-//                st1Lower = true;
-//            }
-//            if(!st2Lower &&  deep1 > deep2) {
-//                st1Lower = true;
-//            }
-//            if(st1Lower || st2Lower) {
-////                Log.info("stack trace diff: st1 size: {}, st2 size: {},\nst1 top: {}, st2 top: {}",deep1,deep2,top1,top2);
-//                testReport.addDiffMethodCall(top1);
-//                testReport.addDiffMethodCall(top2);
-//                diffs.add(new CallDiff(originalStackTrace.getTop2(), Math.abs(deep1 - deep2)));
-//            }
-//
-//            boolean sameTop = top1.equals(top2);
-//            if(st1Lower && st2Lower || !sameTop) {
-////                Log.info("stack trace diff: st1 size: {}, st2 size: {},\nst1 top: {}, st2 top: {}",deep1,deep2,top1,top2);
-//                diffs.add(findNewSyncro(20, 2, originalStackTrace, sosieStackTrace));
-//
-//                if(originalStackTrace.getDeep() == sosieStackTrace.getDeep()) {
-//                    st1Lower = false; st2Lower = false;
-//                }
-//            }
-//            if(sameTop && !(st1Lower && st2Lower)){ //same stack trace
-//                testReport.addSameMethodCall(top1);
-//            }
-//
-//            if(st1Lower == st2Lower && (originalStackTrace.getVariablesValueChange() || sosieStackTrace.getVariablesValueChange())) {
-//                Set<VariableDiff> vd = varDiff(originalStackTrace, sosieStackTrace);
-//                if (!vd.isEmpty()) {
-//                    diffs.addAll(vd);
-//                }
-//            }
-//        }
         originalStackTrace.reset();
         sosieStackTrace.reset();
+
+        boolean st1Lower = false, st2Lower = false;
         while(originalStackTrace.hasNext() && sosieStackTrace.hasNext()) {
-            if (originalStackTrace.nextIsVar() || sosieStackTrace.nextIsVar()) {
+            if (!(originalStackTrace.nextIsVar() || sosieStackTrace.nextIsVar())) {
+                if (!st1Lower) {
+                    originalStackTrace.next();
+                }
+                if (!st2Lower) {
+                    sosieStackTrace.next();
+                }
+            } else {
                 if(originalStackTrace.nextIsVar()) {
 //                    nextVar(originalStackTrace, sosieStackTrace.getStartLogging());
                     originalStackTrace.next();
@@ -89,31 +40,83 @@ public class CompareStackTrace extends AbstractCompareStackTrace {
 //                    nextVar(sosieStackTrace, sosieStackTrace.getStartLogging());
                     sosieStackTrace.next();
                 }
-            } else {
-                sosieStackTrace.next();
-                originalStackTrace.next();
             }
 
             StackTraceCall top1 = originalStackTrace.getTop();
             StackTraceCall top2 = sosieStackTrace.getTop();
-            boolean sameTop = top1.equals(top2);
+            int deep1 = originalStackTrace.getDeep();
+            int deep2 = sosieStackTrace.getDeep();
 
-            if(sameTop) {
-                testReport.addSameMethodCall(top1);
-                if(originalStackTrace.getVariablesValueChange() || sosieStackTrace.getVariablesValueChange()) {
-                    Set<VariableDiff> vd = varDiff(originalStackTrace, sosieStackTrace);
-                    if (!vd.isEmpty()) {
-                        diffs.addAll(vd);
-                    }
-                }
-            } else {
+            if(!st1Lower && deep1 < deep2) {
+                st1Lower = true;
+            }
+            if(!st2Lower &&  deep1 > deep2) {
+                st1Lower = true;
+            }
+            if(st1Lower || st2Lower) {
+//                Log.info("stack trace diff: st1 size: {}, st2 size: {},\nst1 top: {}, st2 top: {}",deep1,deep2,top1,top2);
                 testReport.addDiffMethodCall(top1);
                 testReport.addDiffMethodCall(top2);
+                diffs.add(new CallDiff(originalStackTrace.getTop2(), Math.abs(deep1 - deep2)));
+            }
+
+            boolean sameTop = top1.equals(top2);
+            if(st1Lower && st2Lower || !sameTop) {
 //                Log.info("stack trace diff: st1 size: {}, st2 size: {},\nst1 top: {}, st2 top: {}",deep1,deep2,top1,top2);
                 diffs.add(findNewSyncro(20, 2, originalStackTrace, sosieStackTrace));
 
+                if(originalStackTrace.getDeep() == sosieStackTrace.getDeep()) {
+                    st1Lower = false; st2Lower = false;
+                }
+            }
+            if(sameTop && !(st1Lower && st2Lower)){ //same stack trace
+                testReport.addSameMethodCall(top1);
+            }
+
+            if(st1Lower == st2Lower && (originalStackTrace.getVariablesValueChange() || sosieStackTrace.getVariablesValueChange())) {
+                Set<VariableDiff> vd = varDiff(originalStackTrace, sosieStackTrace);
+                if (!vd.isEmpty()) {
+                    diffs.addAll(vd);
+                }
             }
         }
+//        originalStackTrace.reset();
+//        sosieStackTrace.reset();
+//        while(originalStackTrace.hasNext() && sosieStackTrace.hasNext()) {
+//            if (originalStackTrace.nextIsVar() || sosieStackTrace.nextIsVar()) {
+//                if(originalStackTrace.nextIsVar()) {
+////                    nextVar(originalStackTrace, sosieStackTrace.getStartLogging());
+//                    originalStackTrace.next();
+//                }
+//                if(sosieStackTrace.nextIsVar()) {
+////                    nextVar(sosieStackTrace, sosieStackTrace.getStartLogging());
+//                    sosieStackTrace.next();
+//                }
+//            } else {
+//                sosieStackTrace.next();
+//                originalStackTrace.next();
+//            }
+//
+//            StackTraceCall top1 = originalStackTrace.getTop();
+//            StackTraceCall top2 = sosieStackTrace.getTop();
+//            boolean sameTop = top1.equals(top2);
+//
+//            if(sameTop) {
+//                testReport.addSameMethodCall(top1);
+//                if(originalStackTrace.getVariablesValueChange() || sosieStackTrace.getVariablesValueChange()) {
+//                    Set<VariableDiff> vd = varDiff(originalStackTrace, sosieStackTrace);
+//                    if (!vd.isEmpty()) {
+//                        diffs.addAll(vd);
+//                    }
+//                }
+//            } else {
+//                testReport.addDiffMethodCall(top1);
+//                testReport.addDiffMethodCall(top2);
+////                Log.info("stack trace diff: st1 size: {}, st2 size: {},\nst1 top: {}, st2 top: {}",deep1,deep2,top1,top2);
+//                diffs.add(findNewSyncro(20, 2, originalStackTrace, sosieStackTrace));
+//
+//            }
+//        }
         return diffs;
     }
 }
