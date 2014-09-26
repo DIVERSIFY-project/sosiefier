ce058806a5520530d3da000dc1a8257ed16c7feb
marodrig <marcelino.rguez.cancio@gmail.com>
2014-09-26 11:00:23 +0200

    Some improvements to use the transformations outside the sosie-generator (Idea plugin)

--- src/main/java/fr/inria/diversify/transformation/AbstractTransformation.java
+++ src/main/java/fr/inria/diversify/transformation/AbstractTransformation.java
@@ -1,5 +1,6 @@
 package fr.inria.diversify.transformation;
 
+import fr.inria.diversify.diversification.InputConfiguration;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,7 +13,7 @@ import spoon.reflect.declaration.CtExecutable;
 import spoon.reflect.visitor.FragmentDrivenJavaPrettyPrinter;
 
 import java.util.*;
-import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
 
 /**
  * User: Simon
@@ -48,6 +49,8 @@ public abstract class AbstractTransformation implements Transformation {
     protected String name;
     protected String type;
     protected Transformation parent;
+
+    private InputConfiguration inputConfiguration;
 
     public void setStatus(Integer result) {
         status = result;
@@ -122,24 +125,47 @@ public abstract class AbstractTransformation implements Transformation {
         return parent;
     }
 
-    public void apply(String srcDir) throws Exception {
+    /**
+     * Applies the transformations and saves into the destination directory.
+     * 
+     * @param destinationDir The parent directory to store the transformation. The transformation
+     *                       will try maintain the project structure. For example if we try to
+     *                       apply an transformation over org.pack.myclass, destination dir
+     *                       could be 'modified/src/main/java' and the file will be save in
+     *                       'modified/src/main/java/org/pack/myclass.java'
+     * @throws Exception
+     */
+    public void apply(String destinationDir) throws Exception {
+        
         addSourceCode();
 
-        printJavaFile(srcDir);
+        printJavaFile(destinationDir);
         removeSourceCode();
     }
 
-    public void restore(String srcDir) throws Exception {
+    /**
+     * Restores the original code and stores it in the destination directory
+     *
+     * @param destinationDir The parent directory to store the transformation. The transformation
+     *                       will try maintain the project structure. For example if we try to
+     *                       apply an transformation over org.pack.myclass, destination dir
+     *                       could be 'modified/src/main/java' and the file will be save in
+     *                       'modified/src/main/java/org/pack/myclass.java'
+     * @throws Exception
+     */
+    public void restore(String destinationDir) throws Exception {
         if(parent != null) {
             parent.removeSourceCode();
-            parent.printJavaFile(srcDir);
+            parent.printJavaFile(destinationDir);
         }
         removeSourceCode();
-        printJavaFile(srcDir);
+        printJavaFile(destinationDir);
     }
 
     public void applyWithParent(String srcDir) throws Exception {
+        String processor = getInputConfiguration() == null ? "" : getInputConfiguration().getProperty("processor");
         addSourceCode();
+
         printJavaFile(srcDir);
 
         if (parent != null) {
@@ -165,7 +191,9 @@ public abstract class AbstractTransformation implements Transformation {
         SourcePosition sp = parentMethod.getPosition();
         CompilationUnit compileUnit = sp.getCompilationUnit();
         Environment env = compileUnit.getFactory().getEnvironment();
+
         addSourceCode();
+
 
         FragmentDrivenJavaPrettyPrinter printer = new FragmentDrivenJavaPrettyPrinter(env);
         printer.calculate(compileUnit,null);
@@ -175,8 +203,7 @@ public abstract class AbstractTransformation implements Transformation {
         int begin = sp.getLine() - 1;
         int end = getLineEnd(parentMethod) + code.length - printer.getResult().split("\n").length;
 
-        return Arrays.stream(code, begin, end)
-                .collect(Collectors.joining("\n"));
+        return code + "\n" + begin + "\n" + end + "\n";
     }
 
     protected CtElement getParentMethod(CtElement son) {
@@ -226,5 +253,16 @@ public abstract class AbstractTransformation implements Transformation {
 
     public void setSeries(int series) {
         this.series = series;
+    }
+
+    public void setInputConfiguration(InputConfiguration inputConfiguration) {
+        this.inputConfiguration = inputConfiguration;
+    }
+
+    /**
+     * Global input configuration
+     */
+    public InputConfiguration getInputConfiguration() {
+        return inputConfiguration;
     }
 }
