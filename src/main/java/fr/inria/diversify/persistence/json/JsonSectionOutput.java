ef2ac0264b47530e9c21709fbc7354a767860264
marodrig <marcelino.rguez.cancio@gmail.com>
2015-01-09 11:13:16 +0100

    Refactoring the persistence subsystem

---
+++ src/main/java/fr/inria/diversify/persistence/json/JsonSectionOutput.java
@@ -0,0 +1,48 @@
+package fr.inria.diversify.persistence.json;
+
+import fr.inria.diversify.persistence.PersistenceException;
+import fr.inria.diversify.persistence.SectionOuput;
+import fr.inria.diversify.transformation.Transformation;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by marodrig on 08/01/2015.
+ */
+public abstract class JsonSectionOutput extends SectionOuput {
+
+    public static final String TRANSFORMATIONS = "transformations";
+
+    /**
+     * Resulting global object
+     */
+    JSONObject outputObject;
+
+
+    public void setOutputObject(JSONObject outputObject) {
+        this.outputObject = outputObject;
+    }
+
+    /**
+     * Resulting global object
+     * @return A JSON Object to write to
+     */
+    public JSONObject getOutputObject() {
+        return outputObject;
+    }
+
+
+    @Override
+    public void write(Collection<Transformation> transformations) {
+        if ( getOutputObject() == null ) throw new PersistenceException("JSON Object not set");
+        if ( !getOutputObject().has(TRANSFORMATIONS) ) try {
+            getOutputObject().put(TRANSFORMATIONS, new JSONObject());
+        } catch (JSONException e) {
+            throw new PersistenceException("Cant set transformation object", e);
+        }
+    }
+}
