eadfad3fda912e8062c01aa6938a4c1c2896b867
marodrig <marcelino.rguez.cancio@gmail.com>
2015-01-13 15:46:13 +0100

    Solving VCS errors

---
+++ src/main/java/fr/inria/diversify/persistence/json/input/JsonSectionInput.java
@@ -0,0 +1,59 @@
+package fr.inria.diversify.persistence.json.input;
+
+import fr.inria.diversify.codeFragment.CodeFragment;
+import fr.inria.diversify.diversification.InputProgram;
+import fr.inria.diversify.persistence.PersistenceException;
+import fr.inria.diversify.persistence.SectionInput;
+import org.json.JSONObject;
+
+/**
+ * Created by marodrig on 12/01/2015.
+ */
+public abstract class JsonSectionInput extends SectionInput {
+
+
+
+    /**
+     * JSon to read transformations from
+     */
+    private JSONObject jsonObject;
+
+    /**
+     * Input program where the code fragments of the transformations are
+     */
+    private InputProgram inputProgram;
+
+    /**
+     * Finds a code fragment
+     * @param position Code fragment's position
+     * @param source Code fragment's source
+     * @return  the code fragment
+     */
+    protected CodeFragment getCodeFragment(String position, String source) {
+        CodeFragment c = getInputProgram().getCodeFragment(position, source);
+        if ( c == null ) throw new PersistenceException("Unable to find fragment");
+        return c;
+    }
+
+    /**
+     * JSon to read transformations from
+     */
+    public JSONObject getJsonObject() {
+        return jsonObject;
+    }
+
+    public void setJsonObject(JSONObject jsonObject) {
+        this.jsonObject = jsonObject;
+    }
+
+    /**
+     * Input program where the code fragments of the transformations are
+     */
+    public InputProgram getInputProgram() {
+        return inputProgram;
+    }
+
+    public void setInputProgram(InputProgram inputProgram) {
+        this.inputProgram = inputProgram;
+    }
+}
