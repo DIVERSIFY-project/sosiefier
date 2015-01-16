7c7b4616fad5548dbc7a16566b57fdebc4cc6462
Simon Allier <simon.allier@gmail.com>
2015-01-16 14:32:29 +0100

    update grid5000 script

--- script/grid5000/run.sh
+++ script/grid5000/run.sh
@@ -1,16 +1,3 @@
 #!bin/sh
-rm -r tmpNodeDir
-mkdir tmpNodeDir
-
-rm grid_nodes
-oargridstat -w -l $2 | sort -u > grid_nodes
-
-cd tmpNodeDir
-split -l $1 ../grid_nodes
-
-for nodes in $(ls)
-do
-echo $nodes
-kadeploy3 -f $nodes --multi-server -a ../diversify-exp.env -k http://public.rennes.grid5000.fr/~sallier/authorized_keys &
-done
-cd ..
+oargridstat -w -l $1 | sort -u > grid_nodes
+kadeploy3 -f grid_nodes --multi-server -a diversify-exp.env -k http://public.rennes.grid5000.fr/~sallier/authorized_keys
