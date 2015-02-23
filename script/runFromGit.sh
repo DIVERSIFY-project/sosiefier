987d45e41eddb5b5b5190abc4d25d0b0585278d0
Simon Allier <simon.allier@gmail.com>
2015-02-23 19:59:30 +0100

    update version in script

--- script/runFromGit.sh
+++ script/runFromGit.sh
@@ -4,7 +4,7 @@ for i in `seq 1 $1`
 do
     for j in `seq 1 $cpu`
     do
-        java -Djava.library.path=lib -Xmx8000m  -Dhttp.proxyHost=proxy.rennes.grid5000.fr  -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr  -Dhttp.proxyPort=3128  -jar generator/target/generator-1.0-SNAPSHOT-jar-with-dependencies.jar $(cat propertiesFile) >  out_$i_$j &
+        java -Djava.library.path=lib -Xmx8000m  -Dhttp.proxyHost=proxy.rennes.grid5000.fr  -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr  -Dhttp.proxyPort=3128  -jar generator/target/generator-1.0.0-SNAPSHOT-jar-with-dependencies.jar $(cat propertiesFile) >  out_$i_$j &
     done
     wait
     killall java
