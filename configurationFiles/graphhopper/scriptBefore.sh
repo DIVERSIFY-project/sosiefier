b176aa97de47a8f04bbfbcd02b7ed643a4f38132
Simon Allier <simon.allier@gmail.com>
2015-03-26 20:45:11 +0100

    fix

--- configurationFiles/graphhopper/scriptBefore.sh
+++ configurationFiles/graphhopper/scriptBefore.sh
@@ -2,7 +2,11 @@
 cd  /root/redis-stable/src/
 ./redis-server &
 unset http_proxy
+Xvfb :10 -ac &
+export DISPLAY=:10
 cd /root/diversify-statements
 sh /root/SMART-GH/run_hopper.sh
 sh /root/SMART-GH/run_web.sh &
+unset http_proxy
+
 
