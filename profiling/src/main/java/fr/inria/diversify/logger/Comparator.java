e5d9aa7e5cfec576b1bb1152fefaf9ff36ad0046
Simon Allier <simon.allier@gmail.com>
2015-06-23 16:59:36 +0200

    update

---
+++ profiling/src/main/java/fr/inria/diversify/logger/Comparator.java
@@ -0,0 +1,23 @@
+package fr.inria.diversify.logger;
+
+import fr.inria.diversify.buildSystem.AbstractBuilder;
+import fr.inria.diversify.diversification.InputProgram;
+import fr.inria.diversify.transformation.SingleTransformation;
+import spoon.reflect.cu.SourcePosition;
+
+import java.io.IOException;
+import java.util.Collection;
+
+/**
+ * User: Simon
+ * Date: 23/06/15
+ * Time: 14:36
+ */
+public interface Comparator {
+
+    void init(InputProgram originalInputProgram, AbstractBuilder originalBuilder) throws Exception;
+
+    Object compare(SingleTransformation transformation, String originalLogDir, String sosieLogDir) throws Exception;
+
+    Collection<String> selectTest(SourcePosition position);
+}
