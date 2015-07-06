c1d6f88df2a7b9de30a41e07d0bb875ccc1649d7
Simon Allier <simon.allier@gmail.com>
2015-07-06 11:05:33 +0200

    update script

--- script/runFromGit.sh
+++ script/runFromGit.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
+
+cd /root
+git clone https://github.com/marcelinorc/junco-provider.git
+cd junco-provider
+git pull
+mvn clean install
+
+cd /root/diversify-statements
+git pull
+mvn clean -Dmaven.test.skip=true package
+
+rm -rf repo
+
+java -Djava.library.path=lib -Dhttp.proxyHost=proxy.rennes.grid5000.fr -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr -Dhttp.proxyPort=3128 -jar main/target/main-1.0.0-jar-with-dependencies.jar git repo
+
+
 cpu=$(cat nbProcess)
-for i in `seq 1 $1`
+for i in `seq 1 100000`
 do
     for j in `seq 1 $cpu`
     do
