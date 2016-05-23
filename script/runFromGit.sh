302438bb9b8d9bdd70c5d0eb57514e524bc9eb1f
Simon Allier <simon.allier@gmail.com>
2016-05-23 14:59:17 +0200

    fix

--- script/runFromGit.sh
+++ script/runFromGit.sh
@@ -20,7 +20,7 @@ for i in `seq 1 100000`
 do
  for j in `seq 1 $cpu`
     do
-java -Djava.library.path=lib -Xmx8000m  -Dhttp.proxyHost=proxy.rennes.grid5000.fr  -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr  -Dhttp.proxyPort=3128  -jar main/target/main-1.0.0-jar-with-dependencies.jar $(cat propertiesFile)  # >  out_$i_$j &
+java -Djava.library.path=lib -Xmx8000m  -Dhttp.proxyHost=proxy.rennes.grid5000.fr  -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr  -Dhttp.proxyPort=3128  -jar main/target/main-1.0.0-jar-with-dependencies.jar $(cat propertiesFile)  >  out_$i_$j &
     done
     wait
     killall java
