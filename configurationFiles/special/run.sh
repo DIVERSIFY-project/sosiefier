3f5c5ed4888daf5636a501ee6648741fe821f8f8
Simon Allier <simon.allier@gmail.com>
2016-05-31 10:55:29 +0200

    update

--- configurationFiles/special/run.sh
+++ configurationFiles/special/run.sh
@@ -1,44 +1,44 @@
 #!/bin/sh
 
 
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar breakDelete-codec.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar breakDelete-collections.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar breakDelete-gson.properties
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/breakDelete-codec.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/breakDelete-collections.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/breakDelete-gson.properties
 wait
 killall java
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar breakDelete-io.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar breakDelete-lang.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar checkReturnIf-codec.properties
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/breakDelete-io.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/breakDelete-lang.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/checkReturnIf-codec.properties
 wait
 killall java
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar checkReturnIf-collections.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar checkReturnIf-gson.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar checkReturnIf-io.properties
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/checkReturnIf-collections.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/checkReturnIf-gson.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/checkReturnIf-io.properties
 wait
 killall java
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar checkReturnIf-lang.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar continueDelete-codec.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar continueDelete-collections.properties
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/checkReturnIf-lang.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/continueDelete-codec.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/continueDelete-collections.properties
 wait
 killall java
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar continueDelete-gson.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar continueDelete-io.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar continueDelete-lang.properties
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/continueDelete-gson.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/continueDelete-io.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/continueDelete-lang.properties
 wait
 killall java
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar removeParamterCondition-codec.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar removeParamterCondition-collections.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar removeParamterCondition-gson.properties
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/emoveParamterCondition-codec.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar rconfigurationFiles/special/emoveParamterCondition-collections.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/removeParamterCondition-gson.properties
 wait
 killall java
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar removeParamterCondition-io.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar removeParamterCondition-lang.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar removePut-codec.properties
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/removeParamterCondition-io.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/removeParamterCondition-lang.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/removePut-codec.properties
 wait
 killall java
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar removePut-collections.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar removePut-gson.properties &
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar removePut-io.properties
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/removePut-collections.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/removePut-gson.properties &
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/removePut-io.properties
 wait
 killall java
-java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar removePut-lang.properties
+java -Djava.library.path=lib -Xmx8000m -jar main/target/main-1.0.0-jar-with-dependencies.jar configurationFiles/special/removePut-lang.properties
